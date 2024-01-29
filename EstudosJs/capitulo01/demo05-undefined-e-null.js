faa51c52f828bbd23fe6b50c836131c5c446232b
Givaildo de Andrade <givaildo.oliveira@synvia.com>
2024-01-29 17:59:53 -0300

    Feat: undefined e null

---
+++ EstudosJs/capitulo01/demo05-undefined-e-null.js
@@ -0,0 +1,20 @@
+let variavelSemInicializacao 
+
+console.log(
+    variavelSemInicializacao,
+    typeof(variavelSemInicializacao)
+);
+
+let variavelSemInicializacao = "Hello Word"
+
+console.log(
+    variavelSemInicializacao,
+    typeof(variavelSemInicializacao)
+);
+
+let variavelSemInicializacao = null
+
+console.log(
+    variavelSemInicializacao,
+    typeof(variavelSemInicializacao)
+);
